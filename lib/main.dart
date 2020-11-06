04ff6d6d35883e35cb1ca6de7149f8beed598937
James Ho <jamesho19@gmail.com>
2020-11-07 00:47:47 +0800

    create a project with statefull and stateless widget to load an image

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
-      ),
+          home: Scaffold(
+              backgroundColor: Colors.blue,
+              appBar: AppBar(
+                backgroundColor: Colors.blue.shade700,
+                title: Text('Ask Me anything'),
+              ),
+              body: BallPage())),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Image.asset('images/ball1.png'),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
